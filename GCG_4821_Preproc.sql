675c790a78042975a985137e5a5715fa6dafd03c
Service Web <sw@gssmail.com>
2024-03-12 16:03:53 -0500

    Commit changes in C:\Websites\Git\emarlow\4821\GCG_4821_Preproc.sql

---
+++ GCG_4821_Preproc.sql
@@ -0,0 +1,18 @@
+/* ==========================================
+ * TABLE: GCG_4821_Preproc
+ * ========================================== */
+CREATE TABLE "GCG_4821_Preproc"(
+ "QTY_0" NUMERIC(14,6),
+ "QTY_1" NUMERIC(14,6),
+ "QTY_2" NUMERIC(14,6),
+ "QTY_3" NUMERIC(14,6),
+ "QTY_4" NUMERIC(14,6),
+ "QTY_5" NUMERIC(14,6),
+ "QTY_6" NUMERIC(14,6),
+ "QTY_7" NUMERIC(14,6),
+ "QTY_8" NUMERIC(14,6),
+ "QTY_9" NUMERIC(14,6),
+ "QTY_10" NUMERIC(14,6),
+ "PARENT_QTY_2" NUMERIC(14,6));
+
+
